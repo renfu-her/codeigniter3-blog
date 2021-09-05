ad5eeb8fb001e7e9f7a7f9c1c83af52f40887008
renfu her <renfu.her@gmail.com>
2021-09-05 22:08:26 +0800

    update

--- application/config/hooks.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$hook['post_controller_constructor'] = array(
-    'class'    => 'ManageAuth',
-    'function' => 'auth',
-    'filename' =>'ManageAuth.php',
-    'filepath' => 'hooks'
-);
--- application/hooks/hooks.php
+++
@@ -1,7 +0,0 @@
-<?php
-$hook['post_controller_constructor'] = array(
-    'class'    => 'ManageAuth',
-    'function' => 'auth',
-    'filename' =>'ManageAuth.php',
-    'filepath' => 'hooks'
-);
